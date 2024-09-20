b5f8259585e92d0f7e39ee864b6c17fa364302f9
Erik Poot Sanchez <erikpootsanchez@gmail.com>
2024-09-19 22:56:26 -0600

    Update

--- config/env.js
+++ config/env.js
@@ -5,30 +5,30 @@ const dotenv = require('dotenv');
 const fs = require('fs');
 
 const dotenvFilePath = path.resolve(process.cwd(), '.env');
-
 const AF = /^ARTFOLIO_/i;
 
-const validateEnvFile = async () => {
-    try {
-        if (fs.existsSync(dotenvFilePath)) {
-            dotenv.config({ path: dotenvFilePath });
-        }else{
-            throw new Error(".env NOT FOUND")
-        }
-    } catch (error) {
-        throw new Error(".env NOT FOUND")
+const loadEnvFile = () => {
+  if (fs.existsSync(dotenvFilePath)) {
+    console.log("Loading .env file");
+    dotenv.config({ path: dotenvFilePath });
+  } else {
+    // En producción, las variables de entorno se deben establecer directamente
+    if (process.env.NODE_ENV !== 'production') {
+      console.warn(".env file not found, but proceeding. Make sure environment variables are set.");
     }
+  }
+};
+
+// Solo cargar el archivo .env si no estamos en producción
+if (process.env.NODE_ENV !== 'production') {
+  loadEnvFile();
 }
 
-validateEnvFile()
-
 const env = Object.keys(process.env)
-    .filter((key) => AF.test(key))
-    .reduce(
-        (acc, current) => {
-            acc[current] = process.env[current];
-            return acc;
-        },{}
-    );
+  .filter((key) => AF.test(key))
+  .reduce((acc, current) => {
+    acc[current] = process.env[current];
+    return acc;
+  }, {});
 
 module.exports = env;
