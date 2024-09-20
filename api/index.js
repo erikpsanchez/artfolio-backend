1eee8516609c5cfcc38e8d6512bd35e67644bc77
Erik Poot Sanchez <erikpootsanchez@gmail.com>
2024-09-19 22:11:19 -0600

    Update

--- api/index.js
+++ api/index.js
@@ -1,63 +1,8 @@
-'use strict';
+const express = require("express");
+const app = express();
 
-const express = require('express');
-const router = express.Router();
-const fs = require('fs');
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-const getFoldersAndFiles = (defaultForlderName) => {
-  let files = [];
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-  fs.readdirSync(__dirname, { withFileTypes: true })
-    .filter((dirent) => dirent.isDirectory())
-    .map((dirent) => dirent.name)
-    .forEach((folderName) => {
-      if (
-        !fs.existsSync(
-          `${__dirname}/${folderName}/${defaultForlderName}`,
-        )
-      ) {
-        return [];
-      }
-
-      return fs
-        .readdirSync(
-          `${__dirname}/${folderName}/${defaultForlderName}`,
-        )
-        .forEach((file) => {
-          files.push({ name: folderName, file });
-        });
-    });
-
-  return files;
-};
-
-module.exports = (app) => {
-  getFoldersAndFiles('routes').forEach((folder) => {
-    if (
-      folder['file'].indexOf('.') !== 0 &&
-      folder['file'].slice(-3) === '.js'
-    ) {
-      const {
-        routes,
-      } = require(`${__dirname}/${folder['name']}/routes/${folder['file']}`);
-      routes.forEach((route) => {
-        const { method, path, middleware, action } = route;
-
-        let newRoute = null;
-
-        if (middleware) {
-          let applyMiddleware = Array.isArray(middleware)
-            ? middleware
-            : [middleware];
-
-          newRoute = router[method](path, ...applyMiddleware, action);
-        } else {
-          newRoute = router[method](path, action);
-        }
-
-        app.use('/', newRoute);
-        // console.log(`Route => ${method.toUpperCase()} ${path}`);
-      });
-    }
-  });
-};
+module.exports = app;
